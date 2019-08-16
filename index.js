c8ab9dd2c952d50404894ce701b36f159952c354
sriharinannuru <46162511+sriharinannuru@users.noreply.github.com>
2019-08-16 13:38:44 +0530

    Update index.js
    CHANGED JAYA CODE

--- index.js
+++ index.js
@@ -2,14 +2,8 @@ import $ from 'jquery'
 import Alert from './alert'
 import Button from './button'
 import Carousel from './carousel'
-import Collapse from './collapse'
-import Dropdown from './dropdown'
-import Modal from './modal'
-import Popover from './popover'
-import Scrollspy from './scrollspy'
-import Tab from './tab'
-import Tooltip from './tooltip'
-import Util from './util'
+
+
 
 /**
  * --------------------------------------------------------------------------
